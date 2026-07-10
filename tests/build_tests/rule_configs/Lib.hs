57c432466a4e0c7905b27f861395a35e07b3f4e8
Facundo Domínguez <facundo.dominguez@tweag.io>
2026-07-10 20:35:40 +0000

    Add a non-boot toolchain library dependency when testing haddock
    Change-Id: Iabe7d6231715ad91a0721761e5092745b82b2498

--- tests/build_tests/rule_configs/Lib.hs
+++ tests/build_tests/rule_configs/Lib.hs
@@ -1,4 +1,7 @@
-module Lib (greeting) where
+module Lib (greeting, Example) where
 
+import Test.Hspec (Example)
+
+-- | Just a greeting text. We refer to 'Example' to test haddock.
 greeting :: String
 greeting = "Hello from Lib"
